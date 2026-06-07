3f14afa7b85964693839fda46d917466e9d3ebee
jeremiasV1vas <jeremias.vivas@mi.unc.edu.ar>
2026-06-07 18:33:46 -0300

    Initialize INVERNADERO.asm with configuration and ISR
    Define processor and configuration settings, save context in ISR.

---
+++ INVERNADERO.asm
@@ -0,0 +1,43 @@
+list		p=16f887	; list directive to define processor
+	#include	<p16f887.inc>	; processor specific variable definitions
+
+
+; '__CONFIG' directive is used to embed configuration data within .asm file.
+; The labels following the directive are located in the respective .inc file.
+; See respective data sheet for additional information on configuration word.
+
+	__CONFIG    _CONFIG1, _LVP_OFF & _FCMEN_ON & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
+	__CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
+
+
+
+;***** VARIABLE DEFINITIONS
+w_temp		EQU	0x7D		; variable used for context saving
+status_temp	EQU	0x7E		; variable used for context saving
+pclath_temp	EQU	0x7F		; variable used for context saving
+
+
+;**********************************************************************
+	ORG     0x000             ; processor reset vector
+
+	nop
+  	goto    main              ; go to beginning of program
+
+
+	ORG     0x004             ; interrupt vector location
+
+	movwf   w_temp            ; save off current W register contents
+	movf	STATUS,w          ; move status register into W register
+	movwf	status_temp       ; save off contents of STATUS register
+	movf	PCLATH,w	  ; move pclath register into w register
+	movwf	pclath_temp	  ; save off contents of PCLATH register
+
+; isr code can go here or be located as a call subroutine elsewhere
+
+	movf	pclath_temp,w	  ; retrieve copy of PCLATH register
+	movwf	PCLATH		  ; restore pre-isr PCLATH register contents
+	movf    status_temp,w     ; retrieve copy of STATUS register
+	movwf	STATUS            ; restore pre-isr STATUS register contents
+	swapf   w_temp,f
+	swapf   w_temp,w          ; restore pre-isr W register contents
+	retfie
